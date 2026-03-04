37a8b78656e4aaf336c9a4c147e124aed9beaec5
Lucas Paixao <lucaspaixao_dev@hotmail.com>
2026-03-04 20:04:34 -0300

    Adicionado: toolbar do cabeçalho com estilo do Angular Material e refatoração do campo de busca para utilizar componentes do Angular Material

--- src/app/features/list/components/search-input/search-input.ts
+++ src/app/features/list/components/search-input/search-input.ts
@@ -1,9 +1,11 @@
 import { Component, model } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-search-input',
-  imports: [FormsModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './search-input.html',
   styleUrl: './search-input.scss',
 })
